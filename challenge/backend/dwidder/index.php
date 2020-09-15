e5d9fa0840d845cadfee97f0ab14888292e8a8fb
nadav <tashernadav@gmail.com>
2020-09-16 01:18:53 +0300

    Progress

--- challenge/backend/dwidder/index.php
+++ challenge/backend/dwidder/index.php
@@ -3,43 +3,71 @@
 include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "base" . DIRECTORY_SEPARATOR . "APIs.php";
 
 Base::handle(function ($action, $parameters) {
+
     if ($action === "newUser") {
+        // Validate parameters
         if (!isset($parameters->name) || empty($parameters->name))
             throw new Error("Name parameter error");
+        if (!isset($parameters->handle) || empty($parameters->handle))
+            throw new Error("Handle parameter error");
+        // Create the object
+        $user = new stdClass();
+        $user->name = $parameters->name;
+        $user->handle = $parameters->handle;
         // Return a token
-        return TOken::issue($parameters->name);
+        return Token::issue($user);
     }
+
+    // Validate token parameter
+    if (!isset($parameters->token) || empty($parameters->token))
+        throw new Error("Token parameter error");
+    // Validate token
+    $user = Token::validate($parameters->token);
+    // Unset parameter
+    unset($parameters->token);
+
+    // Check actions
     if ($action === "listDweed") {
         return array_slice(scandir("/opt"), 2);
     }
+
     if ($action === "writeDweed") {
-        if (!isset($parameters->token) || empty($parameters->token))
-            throw new Error("Token parameter error");
-        $userName = Token::validate($parameters->token);
+        // Validate parameters
         if (!isset($parameters->title) || empty($parameters->title))
             throw new Error("Title parameter error");
         if (!isset($parameters->contents) || empty($parameters->contents))
             throw new Error("Contents parameter error");
+
         // Create ID
         $id = bin2hex(time()) . bin2hex(Base::random(4));
+
         // Create JSON
         $file = "/opt/$id";
-        // Unset token parameter
-        unset($parameters->token);
+
         // Set date & user parameter
-        $parameters->name = $userName;
         $parameters->date = date("H:i");
+        $parameters->handle = $user->handle;
+
         // Write
         file_put_contents($file, json_encode($parameters));
+
         return $id;
     }
+
     if ($action === "readDweed") {
+        // Validate parameters
         if (!isset($parameters->id) || empty($parameters->id))
             throw new Error("ID parameter error");
+
+        // Filter parameters
         $dweedId = $parameters->id;
         $dweedId = basename($dweedId);
+
+        // Make sure the dweet exists
         if (!file_exists("/opt/$dweedId"))
             throw new Error("Dweed does not exist");
+
+        // Read dweet
         return json_decode(file_get_contents("/opt/$dweedId"));
     }
     throw new Error("Unknown action");
--- challenge/frontend/application/application.js
+++ challenge/frontend/application/application.js
@@ -11,28 +11,24 @@ window.addEventListener("load", async function () {
     } else {
         let parameters = new URLSearchParams(window.location.search);
         if (parameters.has("dweed")) {
+            UI.clear("dweeds");
             readDweed(parameters.get("dweed")).then((dweed) => {
-                insertDweed(dweed, "big");
+                insertDweed(dweed, "big", "dweeds");
             }).catch(alert);
         } else {
-            API.call("dwidder", "listDweed", {}).then((list) => {
-                for (let id of list.slice(-10)) {
-                    readDweed(id).then((dweed) => {
-                        insertDweed(dweed, "small");
-                    }).catch(alert);
-                }
-            }).catch(console.warn);
+            loadDweeds();
         }
     }
 });
 
-function insertDweed(dweed, style) {
-    UI.find("dweeds").appendChild(UI.populate("dweed-" + style, dweed));
+function insertDweed(dweed, style, container) {
+    UI.find(container).appendChild(UI.populate("dweed-" + style, dweed));
 }
 
 function newUser() {
     API.call("dwidder", "newUser", {
-        name: UI.read("setup-name")
+        name: UI.read("setup-name"),
+        handle: UI.read("setup-handle"),
     }).then((token) => {
         window.localStorage.setItem("token", token.toString());
         window.location.reload();
@@ -52,6 +48,7 @@ function writeDweed() {
 function readDweed(id) {
     return new Promise((resolve, reject) => {
         API.call("dwidder", "readDweed", {
+            token: window.localStorage.getItem("token"),
             id: id
         }).then((dweed) => {
             // Ensure dweed structure
@@ -59,12 +56,39 @@ function readDweed(id) {
                 reject("Missing title property");
             if (!dweed.hasOwnProperty("contents"))
                 reject("Missing contents property");
-            if (!dweed.hasOwnProperty("name"))
-                reject("Missing name property");
+            if (!dweed.hasOwnProperty("handle"))
+                reject("Missing handle property");
             if (!dweed.hasOwnProperty("date"))
                 reject("Missing date property");
             // Resolve
             resolve(dweed);
         }).catch(reject);
     });
+}
+
+function loadDweeds() {
+    UI.clear("dweeds");
+    API.call("dwidder", "listDweed", {
+        token: window.localStorage.getItem("token")
+    }).then((list) => {
+        for (let id of list.slice(-10)) {
+            readDweed(id).then((dweed) => {
+                insertDweed(dweed, "small", "dweeds");
+            }).catch(alert);
+        }
+    }).catch(console.warn);
+}
+
+function loadMentions() {
+    UI.clear("mentions");
+    API.call("dwidder", "listMentions", {
+        token: window.localStorage.getItem("token")
+    }).then((list) => {
+        readDweed(id).then((dweed) => {
+            // Modify dweed
+            dweed.id = id;
+            // Insert dweed
+            insertDweed(dweed, "tiny", "mentions");
+        }).catch(alert);
+    }).catch(console.warn);
 }
